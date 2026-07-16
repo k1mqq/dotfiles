ca9eac37b23a7f22914021d9ec4a52ee26a89784
k1mqq <keemacurry238@gmail.com>
2026-07-16 15:04:55 +0900

    monitor refresh rate fix

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -3,10 +3,15 @@ hl.monitor({
     output = "eDP-1",
     mode = "preferred",
     position = "auto",
-    scale = 1.2,
+    scale = 1.0,
 })
 
-
+hl.monitor({
+    output = "DP-1",
+    mode = "1920x1080@144",
+    position = "auto",
+    scale = 1.0,
+})
 
 -- programs
 local terminal = "ghostty"
